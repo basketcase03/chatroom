45e7717a0953f623f2fc45f6993c18a91a0bd016
basketcase03 <muskankhajuria03@gmail.com>
2020-12-29 16:20:20 +0530

    call receive

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import threading
 import socket
 
 host = '127.0.0.1' #loclahost
-port = 55555
+port = 55556
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
@@ -29,5 +29,23 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
+def receive():
+    while True:
+        client,address = server.accept()
+        print(f"Connected with {str(address)}")
 
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of the client is {nickname}!')
+        broadcast(f'{nickname} joined the chat!'.enode('ascii'))
+        client.send('Conncted to the server!'.enocde('ascii'))
+
+        thread = threading.Thread(target=handle,args=(client,))
+        thread.start()
+
+print("Server is listening...")
+receive()
 
