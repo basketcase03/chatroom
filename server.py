b099d840f0a34c54a798caa41bceeb6664ae1924
basketcase03 <muskankhajuria03@gmail.com>
2020-12-29 17:44:02 +0530

    finalised

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import threading
 import socket
 
-host = '127.0.0.1' #loclahost
-port = 55556
+host = '127.0.0.1' 
+port = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
@@ -13,26 +13,26 @@ nicknames = []
 
 def broadcast(message):
     for client in clients:
-        client.send(messaage)
+        client.send(message)
 
 def handle(client):
     while True:
         try:
-            message = client.rev(1024)
+            message = client.recv(1024)
             broadcast(message)
         except:
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            broadcast("{} left the chat!".format(nickname).encode('ascii'))
             nicknames.remove(nickname)
             break
 
 def receive():
     while True:
         client,address = server.accept()
-        print(f"Connected with {str(address)}")
+        print("Connected with {}".format(str(address)))
 
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
@@ -40,8 +40,8 @@ def receive():
         clients.append(client)
 
         print(f'Nickname of the client is {nickname}!')
-        broadcast(f'{nickname} joined the chat!'.enode('ascii'))
-        client.send('Conncted to the server!'.enocde('ascii'))
+        broadcast("{} joined the chat!".format(nickname).encode('ascii'))
+        client.send('Conncted to the server!'.encode('ascii'))
 
         thread = threading.Thread(target=handle,args=(client,))
         thread.start()
