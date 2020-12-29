ee43e852b0055a03f1960059f5be3f4941a1cde3
basketcase03 <muskankhajuria03@gmail.com>
2020-12-29 15:38:07 +0530

    added server broadcast

---
+++ server.py
@@ -0,0 +1,18 @@
+import threading
+import socket
+
+host = '127.0.0.1' #loclahost
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(messaage)
+
+
